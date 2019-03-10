ae77577aa47fb6e48200d6ed907b18c8f272ad11
lxiiii <lxiiiiii@163.com>
2019-03-10 20:42:29 +0800

    ggf

---
+++ Assets/Scripts/foodMaker.cs
@@ -0,0 +1,55 @@
+﻿
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class foodMaker : MonoBehaviour
+{
+    private static foodMaker _instance;
+    public static foodMaker Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public int xlimit = 21;
+    public int ylimit = 11;
+    public int xoffset = 9;
+    public GameObject foodPrefab;
+    public GameObject rewardPrefab;
+    public Sprite[] foodSprites;
+    private Transform foodHolder;
+
+    
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Start()
+    {
+        foodHolder = GameObject.Find("foodHolder").transform;
+        makeFood(false);
+    }
+
+    public void makeFood(bool isReward)
+    {
+        int index = Random.Range(0, foodSprites.Length);
+        GameObject food = Instantiate(foodPrefab);
+        food.GetComponent<Image>().sprite = foodSprites[index];
+        food.transform.SetParent(foodHolder, false);
+        int x = Random.Range(-xlimit + xoffset, xlimit);
+        int y = Random.Range(-ylimit, ylimit);
+        food.transform.localPosition = new Vector3(x * 30, y * 30, 0);
+        if (isReward)
+        {
+            GameObject reward = Instantiate(rewardPrefab);
+            reward.transform.SetParent(foodHolder, false);
+            x = Random.Range(-xlimit + xoffset, xlimit);
+            y = Random.Range(-ylimit, ylimit);
+            reward.transform.localPosition = new Vector3(x * 30, y * 30, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/snakeHead.cs
@@ -0,0 +1,185 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+//using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class snakeHead : MonoBehaviour
+{
+    public List<Transform> bodyList = new List<Transform>();
+    public float velocity = 0.35f;
+    public int step;
+    private int x;
+    private int y;
+    private Vector3 headPos;
+    private Transform canvas;
+
+    
+    public Sprite[] bodySprites = new Sprite[2];
+
+    public bool isDie = false;
+    public AudioClip eatClip;
+    public AudioClip dieClip;
+    public GameObject dieEffect;
+    public GameObject bodyPrefab;
+
+    void Awake()
+    {
+        canvas = GameObject.Find("Canvas").transform;
+        //通过Resources.Load(string path)方法加载资源，path的书写不需要加Resources/以及文件扩展名
+        gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>(PlayerPrefs.GetString("sh", "sh02"));
+        bodySprites[0] = Resources.Load<Sprite>(PlayerPrefs.GetString("sb01", "sb0201"));
+        bodySprites[1] = Resources.Load<Sprite>(PlayerPrefs.GetString("sb02", "sb0202"));
+    }
+
+    void Start()
+    {
+        InvokeRepeating("Move", 0, velocity);
+        x = 0;y = step;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space)&&MainUIController.Instance.isPause == false&&isDie == false)//按下空格加速
+        {
+            CancelInvoke();
+            InvokeRepeating("Move", 0, velocity - 0.2f);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && MainUIController.Instance.isPause == false && isDie == false)//抬起回到正常速度 
+        {
+            CancelInvoke();
+            InvokeRepeating("Move", 0, velocity);
+        }
+
+        if (Input.GetKey(KeyCode.W) && y != -step && MainUIController.Instance.isPause == false && isDie == false)
+        {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            x = 0;
+            y = step;
+        }
+        if (Input.GetKey(KeyCode.S) && y != step && MainUIController.Instance.isPause == false && isDie == false)
+        {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 180);
+            x = 0;
+            y = -step;
+        }
+        if (Input.GetKey(KeyCode.A) && x != step && MainUIController.Instance.isPause == false && isDie == false)
+        {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 90);
+            x = -step;
+            y = 0;
+        }
+        if (Input.GetKey(KeyCode.D) && x != -step && MainUIController.Instance.isPause == false && isDie == false)
+        {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 0, -90);
+            x = step;
+            y = 0;
+        }
+
+    }
+
+    void Move()
+    {
+        headPos = gameObject.transform.localPosition;                                               //保存下来蛇头移动前的位置
+        gameObject.transform.localPosition = new Vector3(headPos.x + x, headPos.y + y, headPos.z);  //蛇头向期望位置移动
+        if (bodyList.Count > 0)
+        {
+            //由于是双色蛇身，此方法舍
+            //bodyList.Last().localPosition = headPos;                                              //将蛇尾移动到蛇头移动前的位置
+            //bodyList.Insert(0, bodyList.Last());                                                  //将蛇尾在List中的位置更新到最前
+            //bodyList.RemoveAt(bodyList.Count - 1);                                                //移除List最末尾的蛇尾引用
+
+            for (int i = bodyList.Count - 2; i >= 0; i--)                                           //从后往前开始移动蛇身
+            {
+                bodyList[i + 1].localPosition = bodyList[i].localPosition;                          //每一个蛇身都移动到它前面一个节点的位置
+            }
+            bodyList[0].localPosition = headPos;                                                    //第一个蛇身移动到蛇头移动前的位置
+        }
+    }
+
+    void Grow()
+    {
+        AudioSource.PlayClipAtPoint(eatClip, Vector3.zero);
+        int index = (bodyList.Count % 2 == 0) ? 0 : 1;
+        GameObject body = Instantiate(bodyPrefab, new Vector3(2000, 2000, 0), Quaternion.identity);
+        body.GetComponent<Image>().sprite = bodySprites[index];
+        body.transform.SetParent(canvas, false);
+        bodyList.Add(body.transform);
+    }
+
+    void Die()
+    {
+        AudioSource.PlayClipAtPoint(dieClip, Vector3.zero);
+        CancelInvoke();
+        isDie = true;
+        Instantiate(dieEffect);
+        PlayerPrefs.SetInt("lastl", MainUIController.Instance.length);
+        PlayerPrefs.SetInt("lasts", MainUIController.Instance.score);
+        if (PlayerPrefs.GetInt("bests",0)<MainUIController.Instance.score)
+        {
+            PlayerPrefs.SetInt("bestl", MainUIController.Instance.length);
+            PlayerPrefs.SetInt("bests", MainUIController.Instance.score);
+        }
+        StartCoroutine(GameOver(1.5f));
+    }
+
+    IEnumerator GameOver(float t)
+    {
+        yield return new WaitForSeconds(t);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //同collision.tag == "food"
+        if (collision.gameObject.CompareTag("food"))
+        {
+            Destroy(collision.gameObject);
+            MainUIController.Instance.UpdateUI();
+            Grow();
+            foodMaker.Instance.makeFood((Random.Range(0,100)<20) ? true:false);
+            
+        }
+        else if (collision.gameObject.CompareTag("reward"))
+        {
+            Destroy(collision.gameObject);
+            MainUIController.Instance.UpdateUI(Random.Range(5,15)*10);
+            Grow();
+        }
+
+        else if (collision.gameObject.CompareTag("body"))
+        {
+            Die();
+        }
+        else
+        {
+            if (MainUIController.Instance.hasBorder)
+            {
+                Die();
+
+            }
+            else
+            {
+                switch (collision.gameObject.name)
+                {
+                    case "up":
+                        transform.localPosition = new Vector3(transform.localPosition.x, -transform.localPosition.y + 30, transform.localPosition.z);
+                        break;
+                    case "down":
+                        transform.localPosition = new Vector3(transform.localPosition.x, -transform.localPosition.y - 30, transform.localPosition.z);
+                        break;
+                    case "left":
+                        transform.localPosition = new Vector3(-transform.localPosition.x + 185, transform.localPosition.y, transform.localPosition.z);
+                        break;
+                    case "right":
+                        transform.localPosition = new Vector3(-transform.localPosition.x + 280, transform.localPosition.y, transform.localPosition.z);
+                        break;
+                }
+            }
+            
+        }
+
+    }
+}
